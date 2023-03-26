35e847dd9bbea74196973604fb9342b3e92d867d
nadhifaaffiyan <127294844+nadhifaaffiyan@users.noreply.github.com>
2023-03-26 20:21:27 +0700

    menambahkan Class SayaTubeVideo

--- TP_MOD6_1302213035/Program.cs
+++ TP_MOD6_1302213035/Program.cs
@@ -1,3 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-Console.WriteLine("Hello dunia");
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        SayaTubeVideo sayaTubeVideo = new SayaTubeVideo("Tutorial Design by Contract [Nadhifa Affiyan Zalwi]");
+        sayaTubeVideo.IncreasePlayCount(500);
+        sayaTubeVideo.PrintVideoDetails();
+        sayaTubeVideo.IncreasePlayCount(2147483647);
+    }
+}
